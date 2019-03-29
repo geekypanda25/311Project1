bd4a79db5e2133f71b1f4e480c804550d1b4f21f
shlok <shlok@iastate.edu>
2019-03-28 21:03:04 -0500

    new1

---
+++ src/Node.java
@@ -0,0 +1,51 @@
+public class Node {
+
+    int key = -1, color = 1;
+    Node left = nil, right = nil, parent = nil;
+
+    Node(int key) {
+        this.key = key;
+    }
+
+    Node getParent(){
+
+        }
+
+    Node getLeft(){
+
+        }
+
+    Node getRight(){
+
+        }
+
+    int getKey(){
+
+        }
+
+    int getP(){
+
+        }
+
+    int getVal(){
+
+        }
+
+    int getMaxVal(){
+
+        }
+
+    Endpoint getEndpoint(){
+
+        }
+
+    Endpoint getEmax(){
+
+        }
+
+    int getColor(){
+
+        return color;
+
+        }
+}
