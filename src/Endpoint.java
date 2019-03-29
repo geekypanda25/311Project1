a147da4ce0f9addebe0d97afbafe5b297e17a0fc
Benjamin Ferreira <ferreira@iastate.edu>
2019-03-28 21:17:04 -0500

    Implemented the Endpoint class.

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -1,2 +1,27 @@
 public class Endpoint {
+//    The Endpoint class represents an endpoint and its value.
+
+    //    int getValue(): Returns the endpoint value. For example if the Endpoint object
+//    represents the left endpoint of the interval [1, 3], this would return 1.
+    private int value;
+
+
+    /**
+     * Constructs a new Endpoint using the inputted value
+     * @param value
+     * The value of the Endpoint given by intervalInsert()
+     */
+    public Endpoint(int value) {
+        this.value = value;
+    }
+
+
+    /**
+     * Returns the endpoint value of the Endpoint object
+     * @return
+     * The endpoint value
+     */
+    public int getValue() {
+        return value;
+    }
 }
