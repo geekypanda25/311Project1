26bed8215d5ba443b58aac25b12c116d1d077f1c
Benjamin Ferreira <ferreira@iastate.edu>
2019-03-28 21:30:25 -0500

    Initial implementation of the Intervals class.

--- src/Intervals.java
+++ src/Intervals.java
@@ -1,2 +1,28 @@
 public class Intervals {
+
+
+    private int currID;
+
+
+    public Intervals() {
+
+    }
+
+
+    public void intervalInsert(int a, int b) {
+
+    }
+
+
+    public int findPOM() {
+
+        return 0; // temp
+    }
+
+
+    public RBTree getRBTree() {
+
+        return null; // temp
+    }
+
 }
